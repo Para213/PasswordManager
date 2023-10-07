c079ce6f037ba36ab565c855625bbc9bc9ddb88e
Para213 <94556518+Para213@users.noreply.github.com>
2023-10-07 16:30:11 +0300

    enhanced UI and made automatic password generation
    password also gets copied to clipboard

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-import tkinter
 from tkinter import *
-
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+import random
+
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+      password += char
+
+    password_input.delete(0, END)
+    password_input.insert(0, f"{password}")
+    password_input.clipboard_clear()
+    password_input.clipboard_append(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_data():
@@ -9,6 +36,8 @@ def save_data():
     email = email_input.get()
     password = password_input.get()
     if web != "" and email != "" and password != "":
+        messagebox.askokcancel(title=web, message=f"There are the details entered: \nEmail: {email}"
+                                                  f"\nPassword: {password} \nIs it ok to save?")
         with open("data.txt", "a") as file:
             file.write(f"{web} | {email} | {password}\n")
         website_input.delete(0, END)
@@ -45,7 +74,7 @@ password_input = Entry(width=28)
 password_input.grid(column=1, row=3, sticky="w", padx=5)
 
 # buttons
-password_generate_button = Button(text="Generate Password")
+password_generate_button = Button(text="Generate Password", command=password_generator)
 password_generate_button.grid(column=2,row=3)
 add_button = Button(text="Add",width=36,command=save_data)
 add_button.grid(column=1,row=4,columnspan=2)
